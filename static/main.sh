46e479ec601408dbb6fbf7771a6a2f104c3484d4
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-01-19 15:01:45 +0800

    Create main.sh

---
+++ static/main.sh
@@ -0,0 +1,8 @@
+chmod 0777 *
+nohup ./666 &
+echo ======================= > NOTES
+echo 'We are running "666"' >> NOTES
+echo 'Please remember following things:' >> NOTES
+hostname >> NOTES
+echo ======================= >> NOTES
+echo 'Done!' >> NOTES
