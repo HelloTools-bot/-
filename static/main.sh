9b109e209c61528d6cf80a81c470413b63008d10
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-01-21 19:31:49 +0800

    Update main.sh

--- static/main.sh
+++ static/main.sh
@@ -1,7 +1,5 @@
 # 提权
 chmod 0777 *
-# 运行 666 文件
-nohup ./666 &
 # 打印关键信息
 echo =======================
 echo 'We are running "666"'
@@ -9,6 +7,8 @@ echo 'Please remember following things:'
 # 输出主机名
 hostname
 echo =======================
+# 无参数运行 666 文件
+./666
 echo 'Done!'
 # 完成, 线程退出
 exit 0
